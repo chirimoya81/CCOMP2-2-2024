23268f0e413f44361ab4c5ae5b8ee307999c6c1d
Felix Manrique Bejarano <149218649+chirimoya81@users.noreply.github.com>
2024-04-10 08:18:06 -0500

    Update date.h

--- Practica/ejer2/date.h
+++ Practica/ejer2/date.h
@@ -1,11 +1,32 @@
- #include <string>
- 
- class date{
-public:
-explicit date(int day, int year, int month){
+#include <iostream>
+
+class Date{
+    public:
+        explicit Date(int d, int m, int y):
+        {
+            if(y>=1900 && y<=2050){
+                year=y;
+            }
+            if(m>=1 && m<=12){
+                
+            }
+        }
+
+        // get y set
+        int getDay() const{
+            re
+        }
+
+
+        //C
+        bool isLeapYear()
+
+
+        void 
+    private:
     
+        int day{1};
+        int month{1};
+        int year{1500};
+
 }
-
-
-private:
- }
